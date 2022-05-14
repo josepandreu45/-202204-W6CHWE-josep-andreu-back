b7d9c85d7dfc03a309b9aa66d629237885fc124d
josep andreu <josepa45@gmail.com>
2022-05-14 12:54:44 +0200

    Linked to database

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 require("dotenv").config();
+const debug = require("debug")("robots:root");
 const initializeServer = require("./server/initializeServer");
 
-initializeServer(process.env.PORT || 4000);
+const conectDB = require("./db/index");
+
+(async () => {
+  try {
+    await conectDB(process.env.DATABASE);
+    await initializeServer(process.env.PORT || 4000);
+  } catch (error) {
+    debug("Error ocurred");
+  }
+})();
