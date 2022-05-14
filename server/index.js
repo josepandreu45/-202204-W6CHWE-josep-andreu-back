d753cb6eaa1f8521f59bafa525cc609229408395
josep andreu <josepa45@gmail.com>
2022-05-14 19:46:45 +0200

    Installed and added cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const robotsRouters = require("./routers/robotsRouters");
 const { error404, error500 } = require("./middlewares/errors");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
