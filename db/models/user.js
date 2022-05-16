cdde8506943a966e11d58d577944e24c52e44b36
josep andreu <josepa45@gmail.com>
2022-05-16 18:14:44 +0200

    Created schema for users

---
+++ db/models/user.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = model("User", UserSchema, "users");
+
+module.exports = User;
