8600732cda432f7e46fd73c663f4a6f84cba18d4
josep andreu <josepa45@gmail.com>
2022-05-16 20:08:22 +0200

    Created auth middleware

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
 const robotsRouters = require("./routers/robotsRouters");
+const auth = require("./middlewares/auth");
 const { error404, error500 } = require("./middlewares/errors");
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use("/robots", robotsRouters);
+app.use("/robots", auth, robotsRouters);
 
 app.use(error404);
 app.use(error500);
---
+++ server/middlewares/auth.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+  try {
+    if (!authorization.includes("Bearer")) {
+      throw new Error();
+    }
+    const token = authorization.replace("Bearer ", "");
+    const { id } = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = id;
+    next();
+  } catch {
+    const customError = new Error("invalid token");
+    next(customError);
+    customError.statusCode = 401;
+  }
+};
+
+module.exports = auth;
