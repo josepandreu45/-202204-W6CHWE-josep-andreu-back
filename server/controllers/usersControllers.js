09ff1d6f6d9fee55b1c7ca3db98c09ae9175bcd3
josep andreu <josepa45@gmail.com>
2022-05-17 18:13:11 +0200

    Created userscontrollers

---
+++ server/controllers/usersControllers.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../../db/models/User");
+
+const loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username, password });
+  if (!User) {
+    res.status(401).json({ msg: "usuario o contraseña incorrectos" });
+  } else {
+    const userData = {
+      name: user.name,
+      username: user.username,
+    };
+    const rightPassword = await bcrypt.compare(password, user.password);
+
+    if (!rightPassword) {
+      const error = new Error("usuario o contraseña incorrectos");
+      error.code = 403;
+      next(error);
+    } else {
+      const token = jwt.sign(userData, process.env.JWT_SECRET);
+      res.json({ token });
+    }
+  }
+};
+
+module.exports = loginUser;
